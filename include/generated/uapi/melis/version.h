5291f19048c790c924a9b4dc5991debc228cf050
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-03-01 13:15:14 +0800

    add ignore

--- include/generated/uapi/melis/version.h
+++ include/generated/uapi/melis/version.h
@@ -1,9 +1,9 @@
 #define MELIS_VERSION_CODE 198656
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define SDK_UTS_VERSION  "Tue, 01 Mar 2022 12:59:16 +0800"
-#define SDK_COMPILE_TIME "12:59:16"
+#define SDK_UTS_VERSION  "Tue, 01 Mar 2022 13:14:24 +0800"
+#define SDK_COMPILE_TIME "13:14:24"
 #define SDK_COMPILE_BY   "yuzuki"
-#define SDK_GIT_VERSION  "90d407be83d9f25d2b721046b9ac42b8f076f91a"
+#define SDK_GIT_VERSION  "7f0a6cf3abbe875e4d7e38a804cd6dfad915cf01"
 #define TARGET_BOARD_TYPE  "d1s-evb-board"
 #define CPU_TYPE  "sun20iw1p1"
 #define GCC_VERSION      "gcc version 8.4.0 (T-HEAD RISCV Tools V1.10.2 B20201104)"
