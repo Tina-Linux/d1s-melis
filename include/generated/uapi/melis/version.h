0200d5e789996fb0c01d82231836c604caff8e73
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-03-01 14:07:37 +0800

    project: D1-H base test

--- include/generated/uapi/melis/version.h
+++ include/generated/uapi/melis/version.h
@@ -1,10 +1,10 @@
 #define MELIS_VERSION_CODE 198656
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define SDK_UTS_VERSION  "Tue, 01 Mar 2022 13:51:05 +0800"
-#define SDK_COMPILE_TIME "13:51:05"
+#define SDK_UTS_VERSION  "Tue, 01 Mar 2022 14:06:40 +0800"
+#define SDK_COMPILE_TIME "14:06:40"
 #define SDK_COMPILE_BY   "yuzuki"
-#define SDK_GIT_VERSION  "ae1fbaf1920e9a6e745b70f88a84b6d471d1a844"
-#define TARGET_BOARD_TYPE  "d1s-mq"
+#define SDK_GIT_VERSION  "33fce86bbd04de9de8c5654c961b3edd0903f7d2"
+#define TARGET_BOARD_TYPE  "d1-h-evb-board"
 #define CPU_TYPE  "sun20iw1p1"
 #define GCC_VERSION      "gcc version 8.4.0 (T-HEAD RISCV Tools V1.10.2 B20201104)"
 #define LNK_VERSION      "GNU ld (GNU Binutils) 2.32"
