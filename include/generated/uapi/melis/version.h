ae1fbaf1920e9a6e745b70f88a84b6d471d1a844
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-03-01 13:46:35 +0800

    reset HEAD and pass build test

--- include/generated/uapi/melis/version.h
+++ include/generated/uapi/melis/version.h
@@ -1,9 +1,9 @@
 #define MELIS_VERSION_CODE 198656
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define SDK_UTS_VERSION  "Tue, 01 Mar 2022 13:14:24 +0800"
-#define SDK_COMPILE_TIME "13:14:24"
+#define SDK_UTS_VERSION  "Tue, 01 Mar 2022 13:45:27 +0800"
+#define SDK_COMPILE_TIME "13:45:27"
 #define SDK_COMPILE_BY   "yuzuki"
-#define SDK_GIT_VERSION  "7f0a6cf3abbe875e4d7e38a804cd6dfad915cf01"
+#define SDK_GIT_VERSION  "6e63ab554b8e43856cf24ba80a9d0493ee42c1af"
 #define TARGET_BOARD_TYPE  "d1s-evb-board"
 #define CPU_TYPE  "sun20iw1p1"
 #define GCC_VERSION      "gcc version 8.4.0 (T-HEAD RISCV Tools V1.10.2 B20201104)"
