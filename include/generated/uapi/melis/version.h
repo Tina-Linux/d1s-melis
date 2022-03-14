7e4cbdbdb9602cb0a421242c2017dac441e3201d
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-03-14 18:48:07 +0800

    add docs

--- include/generated/uapi/melis/version.h
+++ include/generated/uapi/melis/version.h
@@ -1,9 +1,9 @@
 #define MELIS_VERSION_CODE 198656
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define SDK_UTS_VERSION  "Wed, 02 Mar 2022 18:41:02 +0800"
-#define SDK_COMPILE_TIME "18:41:02"
+#define SDK_UTS_VERSION  "Wed, 02 Mar 2022 19:08:38 +0800"
+#define SDK_COMPILE_TIME "19:08:38"
 #define SDK_COMPILE_BY   "yuzuki"
-#define SDK_GIT_VERSION  "0200d5e789996fb0c01d82231836c604caff8e73"
+#define SDK_GIT_VERSION  "7bdb0360ea60a52c724a2b64b6ad565baf7cc3b0"
 #define TARGET_BOARD_TYPE  "d1s-mq"
 #define CPU_TYPE  "sun20iw1p1"
 #define GCC_VERSION      "gcc version 8.4.0 (T-HEAD RISCV Tools V1.10.2 B20201104)"
