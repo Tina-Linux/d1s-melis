3e41848ce2b4b6526b97eb0d99fc5deff9972d8b
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-03-29 23:00:43 +0800

    delete

--- projects/v459-perf1/startup.sh
+++
@@ -1,6 +0,0 @@
-echo "Execute startup script begin.............."
-
-#insmod d:\mod\display.mod
-#insmod d:\mod\orange.mod
-#insmod d:\apps\desktop.mod
-echo "...............Execute startup script end"
